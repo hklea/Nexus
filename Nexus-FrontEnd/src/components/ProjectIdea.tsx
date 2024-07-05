275204a0bec6b54d0a2c9e17226cbc803447694a
David Keci <dkeci21@epoka.edu.al>
2024-07-05 19:16:28 -0300

    Starting with two component

---
+++ Nexus-FrontEnd/src/components/ProjectIdea.tsx
@@ -0,0 +1,21 @@
+function ProjectIdea() {
+  return (
+    <div className="bg-[#181818] w-[78%] h-[200px] rounded-[50px] flex items-center justify-center">
+      <div className="w-[80%] flex justify-center">
+        <div className="flex justify-center align-middle">
+          <span></span>
+          <span className="text-left mr-[-25px]">
+            <div className="text-white text-[25px] font-bold">Have any project Idea?</div>
+            <div className="text-gray-400">
+                <p>Amet minim mollit non deserunt </p>
+                <p>ullamco est sit aliqua dolor do amet sint</p>
+            </div>
+          </span>
+        </div>
+        <div className="mt-[12px] h-[60px] text-white ml-auto bg-[#9bb5c8] p-[12px] pl-[40px] pr-[40px] rounded-[40px] text-[20px]">Contact Now</div>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectIdea;
