14e826e557d694c2604d6c389e78e4906e2742b6
hklea <khaxhiu21@epoka.edu.al>
2024-07-08 13:09:37 +0200

    Add to portofolio

--- Nexus-FrontEnd/src/components/Portofolio.tsx
+++ Nexus-FrontEnd/src/components/Portofolio.tsx
@@ -1,27 +1,27 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import '../style/Portfolio.css'; // Import your CSS file for styling if needed
 import img1 from '../assets/images/i1.jpeg';
 function Portfolio() {
-    const [currentSlide, setCurrentSlide] = useState(0);
+    const [currentSlide] = useState(0);// const [currentSlide, setCurrentSlide] = useState(0);
     const portfolioItems = [
-        { image: img1, description: 'Project 1 Description' },
-        { image: img1, description: 'Project 2 Description' },
-        { image: img1, description: 'Project 3 Description' },
-        { image: img1, description: 'Project 4 Description' },
-        { image: img1, description: 'Project 5 Description' },
-        { image: img1, description: 'Project 6 Description' },
-        { image: img1, description: 'Project 7 Description' },
-        { image: img1, description: 'Project 8 Description' },
+        { image: img1, description: 'ArtHive' ,details:'A platform for artists to showcase their work and connect with art lovers.'},
+        { image: img1, description: 'Project2 ',details:'A platform for artists to showcase their work and connect with art lovers.' },
+        { image: img1, description: 'Project 3 Description' ,details:'A platform for artists to showcase their work and connect with art lovers.'},
+        { image: img1, description: 'Project 4 Description',details:'A platform for artists to showcase their work and connect with art lovers.' },
+        { image: img1, description: 'Project 5 Description' ,details:'A platform for artists to showcase their work and connect with art lovers.'},
+        { image: img1, description: 'Project 6 Description' ,details:'A platform for artists to showcase their work and connect with art lovers.'},
+        { image: img1, description: 'Project 7 Description',details:'A platform for artists to showcase their work and connect with art lovers.' },
+        { image: img1, description: 'Project 8 Description',details:'A platform for artists to showcase their work and connect with art lovers.' },
         // Add more items as needed
     ];
 
-    const goToPreviousSlide = () => {
-        setCurrentSlide((prevSlide) => (prevSlide > 0 ? prevSlide - 1 : portfolioItems.length - 1));
-    };
+    // const goToPreviousSlide = () => {
+    //     setCurrentSlide((prevSlide) => (prevSlide > 0 ? prevSlide - 1 : portfolioItems.length - 1));
+    // };
 
-    const goToNextSlide = () => {
-        setCurrentSlide((prevSlide) => (prevSlide < portfolioItems.length - 1 ? prevSlide + 1 : 0));
-    };
+    // const goToNextSlide = () => {
+    //     setCurrentSlide((prevSlide) => (prevSlide < portfolioItems.length - 1 ? prevSlide + 1 : 0));
+    // };
 
     return (
         <div className="portfolio-container">
@@ -38,22 +38,27 @@ function Portfolio() {
                         <div className="item-container">
                             <img src={item.image} alt={`Project ${index + 1}`} className="project-image" />
                             <div className="item-description">
-                                {item.description}
-                               
+                                <div className="item-title">{item.description}</div>
                             </div>
+                            <div className="item-details">
+                                <div className="item-title">{item.details}</div>
+                            </div>
+                            <div className='button-container'>
                             <button className="demo-button">Demo</button>
+                            </div>
+                            
                         </div>
                     </div>
                 ))}
             </div>
-            <div className="navigation-buttons">
+            {/* <div className="navigation-buttons">
                 <div className="arrow left" onClick={goToPreviousSlide}>
                     &#8249;
                 </div>
                 <div className="arrow right" onClick={goToNextSlide}>
                     &#8250;
                 </div>
-            </div>
+            </div> */}
         </div>
     );
 }
