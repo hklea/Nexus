d2c080faffe2bea216abef3968afe976dff2ad9a
David <101630664+Davidi24@users.noreply.github.com>
2024-09-04 14:36:48 -0300

    Update signinSignupController.js

--- Nexus-BackEnd/src/controllers/signinSignupController.js
+++ Nexus-BackEnd/src/controllers/signinSignupController.js
@@ -22,7 +22,8 @@ const register = async (req, res) => {
 };
 
 const login = (req, res) => {
-  console.log("aaaa");
+  console.log('Session ID log: ', req.sessionID);
+  console.log('Session log: ', req.session);
   res.status(200).json({ message: "Logged in successfully" });
 };
 
@@ -34,6 +35,8 @@ const logout = (req, res) => {
 const status = (req, res) => {
   console.log(req.body)
   console.log("User: ",req.user)
+  console.log('Session ID:', req.sessionID);
+  console.log('Session:', req.session);
   if (req.isAuthenticated()) {
     console.log(req.user.googleId ? "Logged in with Google" : "Logged in manually");
     res.json({ message: "User is authenticated", user: req.user });
