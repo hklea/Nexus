64534c6aa8eab65907fc1ba62ccb25b07796d550
David <101630664+Davidi24@users.noreply.github.com>
2024-09-05 09:57:32 -0300

    Update sessionConfig.js

--- Nexus-BackEnd/src/config/sessionConfig.js
+++ Nexus-BackEnd/src/config/sessionConfig.js
@@ -1,28 +1,25 @@
 // config/sessionConfig.js
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
-const mongoose = require('mongoose');
-require('dotenv').config(); // Load environment variables
-
-
-
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const mongoose = require("mongoose");
+require("dotenv").config(); // Load environment variables
 
 const sessionConfig = session({
-  
-  secret: process.env.SESSION_SECRET || '12345678', // Replace with your own secret key
+  // Replace with your own secret key
   resave: false, // Set to false to avoid resaving sessions if they are not modified
-  saveUninitialized: false, // Set to false to avoid creating sessions for unauthenticated users
+  // Set to false to avoid creating sessions for unauthenticated users
   store: MongoStore.create({
     mongoUrl: process.env.DATABASE, // MongoDB connection string
     mongooseConnection: mongoose.connection,
   }),
   cookie: {
-    secure: process.env.NODE_ENV === 'production', 
+    secure: process.env.NODE_ENV === "production",
     maxAge: 1000 * 60 * 60 * 24 * 30,
-    httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
-    sameSite: 'strict',
   },
-});
 
+  saveUninitialized: true,
+  secret: process.env.RDS_COOKIE_SECRET,
+  store: testEnvironment ? null : new require("connect-pg-simple")(session)(),
+});
 
 module.exports = sessionConfig;
