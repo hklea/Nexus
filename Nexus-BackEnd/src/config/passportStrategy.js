6db2d5745bb0f20d44987a7203978ae7163028e9
David <101630664+Davidi24@users.noreply.github.com>
2024-09-05 11:22:32 -0300

    Update passportStrategy.js

--- Nexus-BackEnd/src/config/passportStrategy.js
+++ Nexus-BackEnd/src/config/passportStrategy.js
@@ -89,15 +89,7 @@ module.exports = (passport) => {
   });
   
 
-  passport.authenticate('local', (err, user, info) => {
-    if (err) return next(err);
-    if (!user) return res.status(401).json({ message: 'Authentication failed' });
-  
-    req.logIn(user, (err) => {
-      if (err) return next(err);
-      res.status(200).json({ message: 'Logged in successfully', user });
-    });
-  })(req, res, next);
+ 
 
   
 };
