cf93312ea1b32a46170570af087cf39a327012ca
David <101630664+Davidi24@users.noreply.github.com>
2024-09-05 11:42:23 -0300

    Update signinSignupRouter.js

--- Nexus-BackEnd/src/routers/signinSignupRouter.js
+++ Nexus-BackEnd/src/routers/signinSignupRouter.js
@@ -13,9 +13,10 @@ router.post('/login', (req, res, next) => {
     passport.authenticate('local', (err, user, info) => {
       if (err) return next(err);
       if (!user) return res.status(401).json({ message: 'Authentication failed' });
-  
+       
       req.logIn(user, (err) => {
         if (err) return next(err);
+          console.log('Logged in successfully');
         res.status(200).json({ message: 'Logged in successfully', user });
       });
     })(req, res, next);
