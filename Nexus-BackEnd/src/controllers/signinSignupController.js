459fc9203858f78a07a79a515570675aa94b0ce0
David Keci <dkeci21@epoka.edu.al>
2024-09-05 19:40:53 -0300

    ..

--- Nexus-BackEnd/src/controllers/signinSignupController.js
+++ Nexus-BackEnd/src/controllers/signinSignupController.js
@@ -1,6 +1,6 @@
 const User = require("../models/userSchema");
-const { generateToken } = require("../utils/jwtUtils");
-const { verifyToken } = require("../utils/jwtUtils");
+const { generateToken, verifyToken } = require("../utils/jwtUtils");
+const bcrypt = require('bcrypt');
 
 const register = async (req, res) => {
   const { email, password, username } = req.body;
@@ -8,15 +8,25 @@ const register = async (req, res) => {
   try {
     console.log("Registration request body:", req.body);
 
+    // Validate input data here if needed
+
     const userExist = await User.findOne({ email });
 
     if (!userExist) {
-      const user = new User({ email, password, username });
+      // Hash password before saving
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const user = new User({ email, password: hashedPassword, username });
       await user.save();
 
       const token = generateToken(user);
 
-      res.cookie("jwt", token, { httpOnly: true, maxAge: 2592000000 }); // 30 days
+      res.cookie("jwt", token, { 
+        httpOnly: true, 
+        secure: true, // Ensure this is set to true in production
+        sameSite: 'Strict', 
+        maxAge: 2592000000 // 30 days
+      });
       res.status(201).json({ message: "User registered successfully" });
     } else {
       res.status(400).json({ message: "User already exists!" });
@@ -29,15 +39,23 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   const { email, password } = req.body;
+
   try {
     const user = await User.findOne({ email });
 
-    if (!user || !(await user.comparePassword(password))) {
+    if (!user || !(await bcrypt.compare(password, user.password))) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
+
     const token = generateToken(user);
-    res.cookie("jwt", token, { httpOnly: true, maxAge: 2592000000 });
-    console.log("logged in succesfuly")
+
+    res.cookie("jwt", token, { 
+      httpOnly: true, 
+      secure: true, // Ensure this is set to true in production
+      sameSite: 'Strict', 
+      maxAge: 2592000000 // 30 days
+    });
+    console.log("Logged in successfully");
     res.status(200).json({ message: "Logged in successfully" });
   } catch (error) {
     console.error("Error during login:", error);
@@ -46,12 +64,15 @@ const login = async (req, res) => {
 };
 
 const logout = (req, res) => {
-  res.clearCookie("jwt");
+  res.clearCookie("jwt", { 
+    httpOnly: true, 
+    secure: true, // Ensure this is set to true in production
+    sameSite: 'Strict'
+  });
   res.json({ message: "Logged out successfully" });
 };
 
 const checkLoginStatus = async (req, res) => {
-  console.log("Cookies: ",req.cookie)
   const token = req.cookies.jwt; // Retrieve the JWT from cookies
 
   if (!token) {
@@ -67,12 +88,10 @@ const checkLoginStatus = async (req, res) => {
       return res.status(401).json({ message: "User not found" });
     }
 
-    res
-      .status(200)
-      .json({
-        message: "User is logged in",
-        user: { email: user.email, username: user.username },
-      });
+    res.status(200).json({
+      message: "User is logged in",
+      user: { email: user.email, username: user.username },
+    });
   } catch (error) {
     console.error("Error verifying token:", error);
     res.status(401).json({ message: "Invalid or expired token" });
