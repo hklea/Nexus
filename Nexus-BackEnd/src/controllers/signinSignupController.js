6e09cfc63bc671c05d3db772a5b1ba90b8a8d3f8
David Keci <dkeci21@epoka.edu.al>
2024-09-07 11:58:41 -0300

    ..

--- Nexus-BackEnd/src/controllers/signinSignupController.js
+++ Nexus-BackEnd/src/controllers/signinSignupController.js
@@ -20,6 +20,7 @@ const register = async (req, res) => {
       if (userExist.googleId) {
         const user = new User({ password, username });
         await user.save();
+        res.status(201).json({ message: "User registered successfully" });
       } else {
         res.status(400).json({ message: "User already exists!" });
       }
