87a012256830618b4d62a059dfd45948f2cdf55a
David Keci <dkeci21@epoka.edu.al>
2024-09-10 08:56:50 -0300

    ..

--- Nexus-FrontEnd/src/utilities/EntryPage/CallingSocial.tsx
+++ Nexus-FrontEnd/src/utilities/EntryPage/CallingSocial.tsx
@@ -1,12 +1,11 @@
 export const openTikTokPage = () => {
-  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  // const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-  if (isMobile) {
-    // Open TikTok user profile via web link (may open the app if installed)
-    window.location.href = "https://www.tiktok.com/@david";
-  } else {
-    window.open("https://www.tiktok.com/@david", "_blank");
-  }
+  // if (isMobile) {
+  //   window.location.href = "https://www.tiktok.com/@david";
+  // } else {
+  //   window.open("https://www.tiktok.com/@david", "_blank");
+  // }
 };
 
 
@@ -14,10 +13,9 @@ export const openInstagramPage = () => {
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
   if (isMobile) {
-    // Open Instagram user profile directly in the app
-    window.location.href = "instagram://user?username=david__keci";
+    window.location.href = "instagram://user?username=chief.soft24";
   } else {
-    window.open("https://www.instagram.com/david__keci/", "_blank");
+    window.open("https://www.instagram.com/chief.soft24/", "_blank");
   }
 };
 
@@ -26,8 +24,8 @@ export const openInstagramPage = () => {
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
     const whatsappUrl = isMobile
-      ? "https://wa.me/+355677564436" // Mobile-specific URL
-      : "https://web.whatsapp.com/send?phone=+355677564436"; // Web-specific URL
+      ? "https://wa.me/+355677564436" 
+      : "https://web.whatsapp.com/send?phone=+355677564436"; 
 
     window.open(whatsappUrl, "_blank");
   };
